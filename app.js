fbdc59a255997b1b1c50847dcf32cb182102adf6
EC2 Default User <ec2-user@ip-172-31-19-249.us-east-2.compute.internal>
2022-05-20 18:26:56 +0000

    Teste de commit

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set("views", "views");
 
+app.use((req, res, next)=>{
+    res.set('Cache-Control', 'no-store'); 
+    next(); 
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -23,4 +28,4 @@ app.use((req, res, next) => {
   res.status(404).render("404", { pageTitle: "Page Not Found", path: ""});
 });
 
-app.listen(3000);
+app.listen(80);
